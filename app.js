ac0528e34eab03775d3cc9ba5441c0cb28b9b3f9
KECHUN <kechunye@gmail.com>
2023-04-12 15:55:09 -0400

    bcrypt method version

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 //jshint esversion:6
-require("dotenv").config();
+// require("dotenv").config();
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 const express = require("express");
 const https = require("https");
 const app = express();
@@ -9,7 +11,7 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"));
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/userDB");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
 
 const userSchema = new mongoose.Schema ({
   email: String,
@@ -17,14 +19,16 @@ const userSchema = new mongoose.Schema ({
 });
 
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:["password"]});
+// userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:["password"]});
 const User = mongoose.model("User", userSchema);
 
 app.post("/register", function(req, res){
 
+    bcrypt.hash(req.body.password,saltRounds,function(err,hash){
+        
     const user = new User ({
       email: req.body.username,
-      password: req.body.password
+      password: hash
     });
   
     user.save();
@@ -33,21 +37,23 @@ app.post("/register", function(req, res){
 
     });
 
+});
+
 app.post("/login", function(req, res){
 
     
     const userEmail = req.body.username;
-    const userPassword = req.body.password;
+    const passwordInput = req.body.password;
     run()
     async function run(){
     const foundUser = await User.findOne({email:userEmail});
-    if(foundUser && foundUser.password === userPassword){
-        res.render("secrets");
-    }
-        }
-    
-    });
-    
+    if(foundUser) {
+        bcrypt.compare(passwordInput, foundUser.password,function(err,result){
+        if(result===true){
+                res.render("secrets");
+            }})}
+        }   
+    }); 
 
 app.get("/",function(req,res){
     res.render("home")
